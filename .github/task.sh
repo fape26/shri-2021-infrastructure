9a626c2d7d5062c31ee0eb7c9fc0f20e961cc96e
Aleks <26fape@gmail.com>
2021-10-31 00:15:23 +0300

    Update task.sh

--- .github/task.sh
+++ .github/task.sh
@@ -2,5 +2,8 @@
 
 CURRENT_TAG_NAME=$(git tag | tail -1 | head -n1)
 
-echo "CURRENT_TAG_NAME:"
-echo $CURRENT_TAG_NAME
+echo CURRENT_TAG_NAME
+
+curl https://api.tracker.yandex.net/v2/myself -H "Authorization: OAuth ${OAuth}" -H "X-Org-Id: ${OrganizationId}"
+
+echo "CURRENT_TAG_NAME"
