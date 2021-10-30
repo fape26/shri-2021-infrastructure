b31e42f9df35985bd3797b03bd77917a6745cb77
Aleks <26fape@gmail.com>
2021-10-31 00:26:34 +0300

    Update task.sh

--- .github/task.sh
+++ .github/task.sh
@@ -3,10 +3,10 @@
 current_tag=$(git tag | tail -1 | head -n1)
 previous_tag=$(git tag | tail -2 | head -n1)
 
-echo
+
 echo "previous_tag:"
 echo $previous_tag
-echo
+
 echo "current_tag:"
 echo $current_tag
 
