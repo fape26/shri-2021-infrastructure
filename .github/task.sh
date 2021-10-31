26cfa2183ede74985fbd71ed79faef77a41544d0
Aleks <26fape@gmail.com>
2021-10-31 14:40:07 +0300

    Update task.sh

--- .github/task.sh
+++ .github/task.sh
@@ -4,7 +4,7 @@ current_tag=$(git tag | tail -1 | head -n1)
 previous_tag=$(git tag | tail -2 | head -n1)
 author=$(git show ${current_tag} | grep Author: | head -1)
 date=$(git show ${current_tag} | grep Date: | head -1)
-changeLog=$(git log "$previous_tag".. --pretty=format:"%h - %s (%an, %ar)\n" | tr -s "\n" " ")
+descr="${author}\n${date}"
 
 summary="Created release task by lex8329 $current_tag"
 uniqueTag="lex8329/$current_tag"
