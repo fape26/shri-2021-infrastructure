d0f84b6e5a0dc8fa86cd78945a7958c24371a14b
Aleks <26fape@gmail.com>
2021-10-31 14:44:27 +0300

    Update task.sh

--- .github/task.sh
+++ .github/task.sh
@@ -9,7 +9,6 @@ descr="${author}\n${date}"
 summary="Created release task by lex8329 $current_tag"
 uniqueTag="lex8329/$current_tag"
 
-
 postTaskUrl="https://api.tracker.yandex.net/v2/issues/"
 
 headerAuth="Authorization: OAuth ${OAuth}"
@@ -19,14 +18,14 @@ contentType="Content-Type: application/json"
 echo "create Task"
 echo $previous_tag
 
-createTaskRequest=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request POST ${postTaskUrl} \
---header "$headerAuth" \
---header "$headerOrgID" \
---header "$contentType" \
---data-raw '{
-    "queue": "TREK",
-    "summary": "'"{$summary}"'",
-    "type": "task",
-    "description": "'"{$changeLog}"'",
-    "unique": "'"{$uniqueTag}"'"
-}')
+createStatusCode=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request POST ${postTaskUrl} \
+    --header "${headerAuth}" \
+    --header "${headerOrgID}" \
+    --header "${contentType}" \
+    --data-raw '{
+        "queue": "TMP",
+        "summary": "'"${summary}"'",
+        "type": "task",
+        "description": "'"${descr}"'",
+        "unique": "'"${uniqueTag}"'"
+    }')
