bfd85556dd8e469bfa4bc37cc0bfa35e0be385d0
Aleks <26fape@gmail.com>
2021-10-31 17:19:22 +0300

    Create autotest.sh

---
+++ scripts/autotest.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env
+
+current_tag=$(git tag | sort -r | head -n1)
+uniqueTag="lex8329/$current_tag"
+
+findExistingTask="https://api.tracker.yandex.net/v2/issues/_search"
+
+
+
+headerAuth="Authorization: OAuth ${OAuth}"
+headerOrgID="X-Org-Id: ${OrganizationId}"
+contentType="Content-Type: application/json"
+
+testRes=$(npm run test 2>&1)
+
+findTask=$(curl --silent --location --request POST ${findExistingTask} \
+        --header "${headerAuth}" \
+        --header "${headerOrgID}" \
+        --header "${contentType}" \
+        --data-raw '{
+            "filter": {
+                "unique": "'"${uniqueTag}"'"
+              }
+         }' | jq -r '.[0].key')
+echo $findTask
+         
+createCommentURL="https://api.tracker.yandex.net/v2/issues/${findTask}/comments"
+
+comment="Tests:\n${testRes}"
+createComment=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request POST ${createCommentURL} \
+        --header "${headerAuth}" \
+        --header "${headerOrgID}" \
+        --header "${contentType}" \
+        --data-raw '{
+            "text": "'"${comment}"'",
+         }')
+if [ "$createComment" -ne 200 ];
+then
+  echo "Error occurred in attempt to create comment"
+  exit 1
+else
+  echo "Created comment with the result of test run"
+fi
