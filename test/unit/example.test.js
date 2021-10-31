f8b0a3f9f189feca5d0e5dc1751105e77bda4cb5
Aleks <26fape@gmail.com>
2021-10-31 18:37:00 +0300

    Update example.test.js

--- test/unit/example.test.js
+++ test/unit/example.test.js
@@ -2,4 +2,8 @@ describe('Simple Test Case', () => {
     it('Should return 4', () => {
         expect(2 + 2).toBe(4);
     });
+    
+    it('Should return 6', () => {
+        expect(2 * 3).toBe(6);
+    });
 });
