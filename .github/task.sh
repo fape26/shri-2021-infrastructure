cfd7d54435e5f6aa5f71f383e81d617eae860ede
Aleks <26fape@gmail.com>
2021-10-31 14:23:15 +0300

    Update task.sh

--- .github/task.sh
+++ .github/task.sh
@@ -2,12 +2,33 @@
 
 current_tag=$(git tag | tail -1 | head -n1)
 previous_tag=$(git tag | tail -2 | head -n1)
+author=$(git show ${current_tag} | grep Author: | head -1)
+date=$(git show ${current_tag} | grep Date: | head -1)
 
+summary="Created release task by lex8329 $current_tag"
+uniqueTag="lex8329/$current_tag"
+changeLog=$(git log "$previous_tag".. --pretty=format:"%h - %s (%an, %ar)\n" | tr -s "\n" " ")
+
+postTaskUrl="https://api.tracker.yandex.net/v2/issues/"
+
+headerAuth="Authorization: OAuth ${OAuth}"
+headerOrgID="X-Org-Id: ${OrganizationId}"
+contentType="Content-Type: application/json"
+
+responseStatus=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request POST ${postTaskUrl} \
+--header "$headerAuth" \
+--header "$headerOrgID" \
+--header "$contentType" \
+--data-raw '{
+    "queue": "TREK",
+    "summary": "'"${summary}"'",
+    "type": "task",
+    "description": "$changeLog}",
+    "unique": "$uniqueTag"
+}')
 
 echo "previous_tag:"
 echo $previous_tag
 
 echo "current_tag:"
 echo $current_tag
-
-curl https://api.tracker.yandex.net/v2/myself -H "Authorization: OAuth ${OAuth}" -H "X-Org-Id: ${OrganizationId}"
